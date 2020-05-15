ab06bf009802bb81679e914bc85db8d3a5cde37f
Fernando Luis <fernnand.luis@gmail.com>
2020-05-14 21:00:43 -0300

    Adicionado deletar pokemon

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def pokemon(id):
     poke = pokemons[id]
     return render_template(
         'pokemon.html',
-        pokemon=poke
+        pokemon=poke,
+        id=id
     )
 
 @app.route('/add')
@@ -35,6 +36,11 @@ def store():
     img = request.form['img']
     pokemons.append([nome, img])
     
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    del pokemons[id]
     return redirect('/')
 
 if __name__ == '__main__':
